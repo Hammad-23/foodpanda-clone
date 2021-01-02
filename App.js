eb6b2ad37ac4e293930d314eb16e55f2b58ff0b2
Hammad-23 <hsyed2633@gmail.com>
2021-01-03 00:40:33 +0500

    working on drawer on home page

--- App.js
+++ App.js
@@ -1,30 +1,50 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Button,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
 import Home from "./screens/home";
 import RestaurantsDelivery from "./screens/restaurantsDelivery";
-import RestaurantView from './screens/restaurantView'
+import RestaurantView from "./screens/restaurantView";
+import { AntDesign } from "@expo/vector-icons";
+import MyDrawer from "./navigations/drawerNavigation";
+import { useIsDrawerOpen } from "@react-navigation/drawer";
+import { openDrawer } from "@react-navigation/drawer";
 
-export default function App() {
+export default function App({ navigation }) {
   const Stack = createStackNavigator();
   return (
-    // <View style={styles.container}>
-    //   <Text>Hello</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          options={{
+            headerLeft: () => (
+              <AntDesign
+                onPress={() => {
+                  navigation.openDrawer();
+                }}
+                style={{ marginLeft: 10 }}
+                name="bars"
+                size={28}
+                color="black"
+              />
+            ),
+          }}
+          name="Home"
+          component={Home}
+        />
         <Stack.Screen
           name="RestaurantsDelivery"
           component={RestaurantsDelivery}
         />
-        <Stack.Screen
-          name="Restaurant View"
-          component={RestaurantView}
-        />
+        <Stack.Screen name="Restaurant View" component={RestaurantView} />
       </Stack.Navigator>
     </NavigationContainer>
   );
