135036faf1ab420a53cb8a4b79c866371ce5507a
Hammad-23 <hsyed2633@gmail.com>
2021-01-03 19:46:34 +0500

    menu rendering

--- navigations/topTabNavigator/index.js
+++ navigations/topTabNavigator/index.js
@@ -1,16 +1,17 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import Home from "../../screens/home";
 import React from "react";
+import RestaurantMenu from '../../screens/restaurantMenu'
 
 const Tab = createMaterialTopTabNavigator();
 
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="BBQ" component={Home} />
-      <Tab.Screen name="Drinks" component={Home} />
-      <Tab.Screen name="Burgers" component={Home} />
-      <Tab.Screen name="Pizzas" component={Home} />
+      <Tab.Screen name="BBQ" component={RestaurantMenu} />
+      <Tab.Screen name="Drinks" component={RestaurantMenu} />
+      <Tab.Screen name="Burgers" component={RestaurantMenu} />
+      <Tab.Screen name="Pizzas" component={RestaurantMenu} />
     </Tab.Navigator>
   );
 }
--- screens/restaurantMenu/index.js
+++ screens/restaurantMenu/index.js
@@ -10,15 +10,84 @@ import {
 } from "react-native";
 
 function RestaurantMenu() {
-  return(
-      <ScrollView>
-
-
-
-
-
-          
-      </ScrollView>
-  )
+  let menu = [
+    {
+      dish: "Zinger Burger",
+      dishImg: require("../../assets/images/burger2.png"),
+    },
+    {
+      dish: "Zinger Burger",
+      dishImg: require("../../assets/images/burger2.png"),
+    },
+    {
+      dish: "Zinger Burger",
+      dishImg: require("../../assets/images/burger2.png"),
+    },
+    {
+      dish: "Zinger Burger",
+      dishImg: require("../../assets/images/burger2.png"),
+    },
+    {
+      dish: "Zinger Burger",
+      dishImg: require("../../assets/images/burger2.png"),
+    },
+    {
+      dish: "Zinger Burger",
+      dishImg: require("../../assets/images/burger2.png"),
+    },
+    {
+      dish: "Zinger Burger",
+      dishImg: require("../../assets/images/burger2.png"),
+    },
+    {
+      dish: "Zinger Burger",
+      dishImg: require("../../assets/images/burger2.png"),
+    },
+    {
+      dish: "Zinger Burger",
+      dishImg: require("../../assets/images/burger2.png"),
+    },
+  ];
+  return (
+    <ScrollView>
+      {menu.map((item) => {
+        return (
+          <View style={styles.dishRow}>
+            <View style={{ marginLeft: 20 }}>
+              <Text style={styles.dishHead}>{item.dish}</Text>
+              <Text style={{ marginTop: 6 }}>
+                Chicken,Mayo,Ketchup,Kentucky
+              </Text>
+            </View>
+            <View style={{ marginRight: 20 }}>
+              <Image style={styles.img2} source={item.dishImg} />
+            </View>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
 }
 export default RestaurantMenu;
+
+const styles = StyleSheet.create({
+  img2: {
+    width: 60,
+    height: 60,
+  },
+  dishRow: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: "black",
+    borderBottomWidth: 0.5,
+    marginTop: 20,
+    height: 80,
+  },
+  dishHead: {
+    fontSize: 20,
+    fontWeight: "200",
+    fontFamily: "sans-serif",
+  },
+});
